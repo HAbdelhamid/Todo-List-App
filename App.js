9a3520583f2c563f62656caec1dbb3debb4591ad
Hamza Abdelhamid <abdelhamidhamzaa@gmail.com>
2022-03-03 04:09:44 +0100

    Add: styling todo list compenents

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ const todoList = document.querySelector(".todo-list");
 
 //Event Listeners
 todoButton.addEventListener('click', addTodo);
-
+todoList.addEventListener('click', deleteCheck);
 
 //Functions
 
@@ -17,7 +17,7 @@ function addTodo(e) {
     todoDiv.classList.add('todo');
     //Create LI
     const newTodo = document.createElement('li');
-    newTodo.innerText = 'hey';
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
     //Check mark button
@@ -28,9 +28,14 @@ function addTodo(e) {
     //Check trash button
     const trashButton = document.createElement('button');
     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-    trashButton.classList.add("complete-btn");
+    trashButton.classList.add("trash-btn");
     todoDiv.appendChild(trashButton);
     //append TO LIST
     todoList.appendChild(todoDiv);
+    //Clear todo input value
+    todoInput.value = "";
+}
 
+function deleteCheck(e) {
+    
 }
